fb08cbcbb02409cf31bd75c858dafe5cc72c04d9
Dennis Dyatlov <dennisd@bk.ru>
2022-12-15 15:17:12 +0000

    Update InternalsVisibleTo.cs

--- src/OneBeyond.Studio.Hosting/InternalsVisibleTo.cs
+++ src/OneBeyond.Studio.Hosting/InternalsVisibleTo.cs
@@ -1,5 +1,4 @@
 using System.Runtime.CompilerServices;
-using System.Text;
 
 [assembly: InternalsVisibleTo("OneBeyond.Studio.Hosting.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
