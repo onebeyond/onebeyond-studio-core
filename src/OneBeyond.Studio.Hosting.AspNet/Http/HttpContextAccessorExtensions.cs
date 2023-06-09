3d281e3e6046a060dc93df4f73ba6140ce35d275
Fabio Maistro <fabio.maistro@one-beyond.com>
2023-06-09 11:39:56 +0100

    Added TryGetUserClaimValue to HttpContextAccessorExtensions (#46)
    - Added `TryGetUserClaimValue` to `HttpContextAccessorExtensions`
    - Replaced `GetUserAuthId` (kept as obsolete) with `GetUserClaimValue`

--- src/OneBeyond.Studio.Hosting.AspNet/Http/HttpContextAccessorExtensions.cs
+++ src/OneBeyond.Studio.Hosting.AspNet/Http/HttpContextAccessorExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Security.Claims;
 using System.Security.Principal;
 using EnsureThat;
@@ -8,7 +9,27 @@ namespace OneBeyond.Studio.Hosting.AspNet.Http;
 
 public static class HttpContextAccessorExtensions
 {
-    public static string GetUserAuthId(this IHttpContextAccessor httpContextAccessor, string claimType)
+    public static bool TryGetUserClaimValue(
+        this IHttpContextAccessor httpContextAccessor, 
+        string claimType,
+        [NotNullWhen(true)] out string? value)
+    {
+        EnsureArg.IsNotNull(httpContextAccessor, nameof(httpContextAccessor));
+        EnsureArg.IsNotNullOrWhiteSpace(claimType, nameof(claimType));
+
+        var httpContext = httpContextAccessor.HttpContext
+            ?? throw new InvalidOperationException("Current execution context is not bound to any HTTP request.");
+
+        var userIdentity = httpContext.User.Identity
+            ?? throw new InvalidOperationException("Current user is not identified.");
+
+        value = TryGetClaimFromIdentity(userIdentity, claimType);
+        return value != null;
+    }
+
+    public static string GetUserClaimValue(
+        this IHttpContextAccessor httpContextAccessor, 
+        string claimType)
     {
         EnsureArg.IsNotNull(httpContextAccessor, nameof(httpContextAccessor));
         EnsureArg.IsNotNullOrWhiteSpace(claimType, nameof(claimType));
@@ -22,7 +43,15 @@ public static class HttpContextAccessorExtensions
         return GetClaimFromIdentity(userIdentity, claimType);
     }
 
-    private static string GetClaimFromIdentity(IIdentity identity, string type)
+    [Obsolete("This method will soon be deleted, please use GetUserClaimValue instead.")]
+    public static string GetUserAuthId(
+        this IHttpContextAccessor httpContextAccessor, 
+        string claimType)
+        => GetUserClaimValue(httpContextAccessor, claimType);
+
+    private static string GetClaimFromIdentity(
+        IIdentity identity, 
+        string type)
     {
         var claimIdentity = identity as ClaimsIdentity
             ?? throw new InvalidOperationException("Current user does not contain claims.");
@@ -30,4 +59,9 @@ public static class HttpContextAccessorExtensions
             ?? throw new InvalidOperationException($"Current user does not contain claim for {type}.");
         return claim.Value;
     }
+
+    private static string? TryGetClaimFromIdentity(
+        IIdentity identity, 
+        string type) 
+        => (identity as ClaimsIdentity)?.FindFirst(type)?.Value;
 }
