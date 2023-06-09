cfc89baac394494615822e42ce54bab44f9754fb
fabiomaistro <fabio.maistro87@gmail.com>
2023-06-09 11:29:36 +0100

    Fix

--- src/OneBeyond.Studio.Hosting.AspNet/Http/HttpContextAccessorExtensions.cs
+++ src/OneBeyond.Studio.Hosting.AspNet/Http/HttpContextAccessorExtensions.cs
@@ -43,7 +43,7 @@ public static class HttpContextAccessorExtensions
         return GetClaimFromIdentity(userIdentity, claimType);
     }
 
-    [Obsolete("This method will soon be deleted, please GetUserClaimValue instead.")]
+    [Obsolete("This method will soon be deleted, please use GetUserClaimValue instead.")]
     public static string GetUserAuthId(
         this IHttpContextAccessor httpContextAccessor, 
         string claimType)
