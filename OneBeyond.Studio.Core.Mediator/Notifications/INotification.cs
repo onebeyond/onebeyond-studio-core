dfd11cd5b10b6b649a71d9c75fd840759dc240e1
Nick Skliar-Davies <122090431+nickskliar-davies@users.noreply.github.com>
2025-12-08 16:42:47 +0000

    Actually Delete the files. (#154)
    * sys: v4 codeql does noot exist
    
    * fix: v3 also desn't exist
    
    * feature: hand-rolled interfaces
    
    * feature: an implemented mediator
    
    * feature: superior folder structure
    
    feature: start removing
    
    * feature: start unpicking mediatr
    
    * sys: version that builds without mediatr
    
    * feature: remove remaining mediators
    
    * fix: some DI errors
    
    * fix: DI scope and generic handling
    
    * refactor: better naming
    
    * refactor: docstrings
    
    * refactor: fail-fast
    
    * docs: docstrings for DI
    
    * sys: move mediator to correct file location
    
    * refactor: remove redundant code
    
    * fix: references

--- OneBeyond.Studio.Core.Mediator/Notifications/INotification.cs
+++
@@ -1,6 +0,0 @@
-namespace OneBeyond.Studio.Core.Mediator.Notifications;
-
-/// <summary>
-/// Notification - sends a message to all listeners.
-/// </summary>
-public interface INotification : IRequest { } 
--- OneBeyond.Studio.Core.Mediator/Queries/IQuery.cs
+++
@@ -1,8 +0,0 @@
-namespace OneBeyond.Studio.Core.Mediator.Queries;
-
-/// <summary>
-/// Query - for data retrieval only
-/// </summary>
-/// <typeparam name="TResult">Type of result</typeparam>
-public interface IQuery<out TResult> : IRequest
-{ } 
