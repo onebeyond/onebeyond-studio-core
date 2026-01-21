1c75178f38590d479377154ab2279becbc5ee233
Andrii Kaplanovskyi <andrii.kaplanovskyi@One-Beyond.com>
2026-01-21 14:08:16 +0000

    .

--- src/OneBeyond.Studio.Core.Mediator/Pipelines/IMediatorPipelineBehaviour.cs
+++ src/OneBeyond.Studio.Core.Mediator/Pipelines/IMediatorPipelineBehaviour.cs
@@ -1,7 +1,5 @@
 namespace OneBeyond.Studio.Core.Mediator.Pipelines;
 
-//TODO RENAME
-
 public interface IMediatorPipelineBehaviour<in TInput, TOutput>
 {
     public Task<TOutput> HandleAsync(TInput input, Func<Task<TOutput>> next, CancellationToken cancellationToken = default);    
